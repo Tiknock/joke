a775aa6925e845660e6a431831d8f09f2a88f0a7
Tiknock <108576415+Tiknock@users.noreply.github.com>
2023-04-16 00:16:40 +0200

    Add files via upload

--- index.js
+++ index.js
@@ -1,26 +1,33 @@
-app = document.querySelector('.app')
+app = document.querySelector(".app");
 
 function getJoke() {
-fetch("https://api.blablagues.net/?rub=blagues")
-    .then((res) => res.json()
-    ).then((data) => {
-        const joke = data.data.content;
+  fetch("https://api.blablagues.net/?rub=blagues")
+    .then((res) => res.json())
+    .then((data) => {
+      const joke = data.data.content;
 
-        header.textContent = joke.text_head
-        if(joke.text_hidden) {
-        content.textContent= joke.text_hidden
-        } else {
-        content.textContent= joke.text
-        content.style.opacity = 1
-        }
-    })
+      next.style.display = "none";
+      header.textContent = joke.text_head;
+      if (joke.text_hidden) {
+        res.style.display = null;
+        res.addEventListener("click", () => {
+          content.textContent = joke.text_hidden;
+          next.style.display = null;
+          res.style.display = "none";
+        });
+      } else {
+        next.style.display = null;
+        res.style.display = "none";
+        content.classList.remove("showRes");
+        content.textContent = joke.text;
+      }
+    });
 }
 
-getJoke()
-app.addEventListener("click", () => {
-    content.style.opacity = 1
-})
+getJoke();
+
 next.addEventListener("click", () => {
-    content.style.opacity = 0
-    getJoke();  
-})
+  header.textContent = "Recherche d'une blague...";
+  content.textContent = "";
+  getJoke();
+});
